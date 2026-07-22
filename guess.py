b85edf4ec484bf5f7c9c84a7b54d9e76f2ccbe46
Aditya bhat <aditya24.mitmpl2026@learner.manipal.edu>
2026-07-22 12:52:46 +0530

    Update guess.py
    sus

--- guess.py
+++ guess.py
@@ -1,10 +1,13 @@
 import random
+import sys
 print("Choose number between 0 and 9! Guessing Game Begins")
 x = int(input("enter number:"))
 if x>9:
     print("0 to 9 only please!")
+    sys.exit()
 elif x<0:
     print("0 to 9 only please!")
+    sys.exit()
 n = random.randrange(0,9)
 print("My number is:",n)
 if x == n:
