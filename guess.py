4e23322614d19562587b7a0ee8c794e7a6c27b1f
Aditya bhat <aditya24.mitmpl2026@learner.manipal.edu>
2026-07-23 14:30:34 +0530

    Update guess.py

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ import sys
 print("Choose number between 0 and 9! Guessing Game Begins")
 x = int(input("enter number:"))
 if x<0 or x>9:
-    print("Error! , number musgt be within the range 0-9")
+    print("Error! , number must be within the range 0-9")
     sys.exit()
 n = random.randrange(0,9)
 print("My number is:",n)
